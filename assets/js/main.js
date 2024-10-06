acdaf7831b81702d42fe9ef034781af13c043e53
Nicotes369 <nicola369edison@gmail.com>
2024-10-06 23:11:28 +0900

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// Copy Contract Address Function
+function copyAddress() {
+    const address = document.getElementById('contract-address').innerText.split(': ')[1];
+    navigator.clipboard.writeText(address).then(() => {
+        alert('Contract address copied to clipboard!');
+    });
+}
