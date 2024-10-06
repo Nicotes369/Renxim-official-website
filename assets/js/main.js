d54a1a01db64fc755210d53e1158231facac80b7
Nicotes369 <nicola369edison@gmail.com>
2024-10-06 23:26:20 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,8 @@ function copyAddress() {
     const address = document.getElementById('contract-address').innerText.split(': ')[1];
     navigator.clipboard.writeText(address).then(() => {
         alert('Contract address copied to clipboard!');
+    }).catch(err => {
+        alert('Failed to copy address.');
+        console.error('Error copying text: ', err);
     });
 }
